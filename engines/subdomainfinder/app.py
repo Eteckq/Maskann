ae5ac5adeb55925308a998a05712ac0c15a52cc3
Ganko <HansM2013@gmx.de>
2024-02-19 12:16:38 +0100

    Gestion erreur domaine subdomain list

--- engines/subdomainfinder/app.py
+++ engines/subdomainfinder/app.py
@@ -7,7 +7,7 @@ import sublist3r
 
 sys.path.insert(0, os.path.join(os.path.dirname(sys.path[0])))
 from base_engine.engine import Engine, Assets
-from base_engine.utils import execute_cmd
+from base_engine.utils import is_domain
 
 
 class Options(BaseModel):
@@ -18,18 +18,18 @@ class SubDomainFinder(Engine):
     def start_scan(self, assets: Assets, options: None, scan):
         results = []
         for asset in assets.values:
-            # if is_url(asset):
-            scan_result = sublist3r.main(
-                asset,
-                40,
-                "/dev/null",
-                ports=None,
-                silent=False,
-                verbose=False,
-                enable_bruteforce=False,
-                engines=None,
-            )
-            results.append({"asset": asset, "scan": scan_result})
+            if is_domain(asset):
+                scan_result = sublist3r.main(
+                    asset,
+                    40,
+                    "/dev/null",
+                    ports=None,
+                    silent=False,
+                    verbose=False,
+                    enable_bruteforce=False,
+                    engines=None,
+                )
+                results.append({"asset": asset, "scan": scan_result})
         return results
 
     def example_reponse(self):

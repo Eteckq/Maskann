3947cd973a2179a0310567b3757779a134384f17
Ganko <HansM2013@gmx.de>
2024-02-19 17:15:10 +0100

    Add dig engine

---
+++ engines/dig/app.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel
+import sys
+import os
+from os.path import isfile, join
+import pydig
+
+sys.path.insert(0, os.path.join(os.path.dirname(sys.path[0])))
+from base_engine.engine import Engine, Assets
+from base_engine.utils import is_domain
+from enum import Enum
+
+
+class TypeDig(str, Enum):
+    A = "A"
+    AAAA = "AAAA"
+    CAA = "CAA"
+    CNAME = "CNAME"
+    DNSKEY = "DNSKEY"
+    DS = "DS"
+    MX = "MX"
+    NS = "NS"
+    PTR = "PTR"
+    SOA = "SOA"
+    SRV = "SRV"
+    TLSA = "TLSA"
+    TSIG = "TSIG"
+    TXT = "TXT"
+
+
+class Options(BaseModel):
+    type_dig: TypeDig
+
+
+class SubDomainFinder(Engine):
+    def start_scan(self, assets: Assets, options: Options, scan):
+        results = []
+        for asset in assets.values:
+            if is_domain(asset):
+                scan_result = pydig.query(asset, options.type_dig)
+                results.append(
+                    {"asset": asset, "dig_type": options.type_dig, "scan": scan_result}
+                )
+        return results
+
+    def example_reponse(self):
+        return {
+            "asset": "yohangastoud.fr",
+            "dig_type": "SOA",
+            "scan": ["dns20.ovh.net. tech.ovh.net. 2023050700 86400 3600 3600000 300"],
+        }
+
+
+engine = SubDomainFinder(Options)
